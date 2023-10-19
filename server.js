eef49147a77512436093c2543c5ad82345f8e456
Nick Wooten <nswooten@gmail.com>
2023-10-19 13:01:17 -0500

    establish database connection

--- server.js
+++ server.js
@@ -1,48 +1,36 @@
-// import npm packages
+// npm packages
+import 'dotenv/config.js'
 import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import createError from 'http-errors'
 import logger from 'morgan'
+import cors from 'cors'
+import formData from 'express-form-data'
 
-// import routers
-import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+// connect to MongoDB with mongoose
+import './config/database.js'
+
+// import routes
+import {router as usersRouter } from "./routes/users.js"
 
 // create the express app
 const app = express()
 
-// view engine setup
-app.set('view engine', 'ejs')
-
 // basic middleware
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(
-  express.static(
-    path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
-  )
-)
+app.use(formData.parse())
 
 // mount imported routes
-app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
-// catch 404 and forward to error handler
+// handle 404 errors
 app.use(function (req, res, next) {
-  next(createError(404))
+  res.status(404).json({ err: 'Not found' })
 })
 
-// error handler
+// handle all other errors
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  res.status(err.status || 500).json({ err: err.message })
 })
 
 export { app }
